b86c4e9f94af4474dada06abfc875bf6e50d2517
Christoph Buchner <bilderbuchi@phononoia.at>
2014-03-26 22:02:32 +0100

    CI server: Add a stub script for code styling.

---
+++ scripts/dev/CI/style_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Code styling script for CI builds
+
+# Desired feature list:
+# * If triggered by PR build, only style the files which changed in the PR
+#   This can probably be done with some smart git magic
+# * Make sure the script can meaningfully be called locally, too.
+
+# Still to be implemented, remains a no-op for now
+exit 0
