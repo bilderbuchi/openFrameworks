34403acc8101cdd2174a444a5702fa1b3c9c99e6
Christoph Buchner <bilderbuchi@phononoia.at>
2014-04-13 13:08:43 +0200

    Add some info to styling script.

--- scripts/dev/CI/style_files.sh
+++ scripts/dev/CI/style_files.sh
@@ -4,8 +4,9 @@
 
 # Desired feature list:
 # * If triggered by PR build, only style the files which changed in the PR
-#   This can probably be done with some smart git magic
+#   This can probably be done with some smart git magic and giving a list of files to an uncrustify script (which should filter the list to screen files we don't want to style, e.g. in external library code)
 # * Make sure the script can meaningfully be called locally, too.
 
-# Still to be implemented, remains a no-op for now
+echo "Styling touched files with OF code style"
+echo "Not yet implemented, still a no-op."
 exit 0
