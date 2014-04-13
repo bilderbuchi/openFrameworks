050117e0465e69cce0ee9b2d8cb7137f3cf059f5
Christoph Buchner <bilderbuchi@phononoia.at>
2014-04-13 11:55:25 +0200

    Restructure Ubuntu install scripts to run only apt-get as sudo.

--- scripts/linux/ubuntu/install_codecs.sh
+++ scripts/linux/ubuntu/install_codecs.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ $EUID != 0 ]; then
-	echo "this script must be run using sudo"
-	echo ""
-	echo "usage:"
-	echo "sudo ./install_dependencies.sh"
-	exit $exit_code
-   exit 1
-fi
-
 if [ "$1" == "--unattended" ]; then
     echo "Running script unattended"
     APTGET_OPTION="-yq"
@@ -24,4 +15,11 @@ if [ "$RET" -eq "0" ]; then
     GSTREAMER_VERSION=1.0
 fi
 
-apt-get $APTGET_OPTION install libmpg123-dev gstreamer${GSTREAMER_VERSION}-plugins-ugly
+echo "Sudo permissions are needed for running the package manager."
+sudo apt-get $APTGET_OPTION install libmpg123-dev gstreamer${GSTREAMER_VERSION}-plugins-ugly
+exit_code=$?
+if [ $exit_code != 0 ]; then
+    echo "error installing codecs, there could be an error with your internet connection"
+    echo "if the error persists, please report an issue in github: http://github.com/openframeworks/openFrameworks/issues"
+	exit $exit_code
+fi
