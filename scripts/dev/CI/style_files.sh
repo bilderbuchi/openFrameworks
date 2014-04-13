446af67b1123a13ef81c6327c6f8f719707e7826
Christoph Buchner <bilderbuchi@phononoia.at>
2014-04-13 21:04:51 +0200

    dummy commit

--- scripts/dev/CI/style_files.sh
+++ scripts/dev/CI/style_files.sh
@@ -6,7 +6,7 @@
 # * If triggered by PR build, only style the files which changed in the PR
 #   This can probably be done with some smart git magic and giving a list of files to an uncrustify script (which should filter the list to screen files we don't want to style, e.g. in external library code)
 # * Make sure the script can meaningfully be called locally, too.
-
+      
 echo "Styling touched files with OF code style"
 echo "Not yet implemented, still a no-op."
 exit 0
