6c5e7f760c2f8cc76439f2b1eac928c0bcec3a76
Christoph Buchner <bilderbuchi@users.noreply.github.com>
2015-07-17 13:41:37 +0200

    some instrumentation on linux testtemplate

--- scripts/linux/testTemplate.sh
+++ scripts/linux/testTemplate.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -e
+set -ev
+echo $OF_ROOT
 cd $OF_ROOT
+pwd
+ls -la
 cp scripts/linux/template/linux/Makefile examples/empty/emptyExample/
 cp scripts/linux/template/linux/config.make examples/empty/emptyExample/
 cd examples/empty/emptyExample
